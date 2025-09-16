aefb48731a4447e8fd6e711e782e16d7bc7154e6
MeowZ#WR <antabakala@hotmail.com>
2025-09-16 19:05:02 +0800

    About

--- PunishLib/AboutPlugin.cs
+++ PunishLib/AboutPlugin.cs
@@ -29,6 +29,17 @@ namespace PunishLib
                 ImGui.SameLine(0, 0);
                 ImGuiEx.Text($" by Puni.sh");
             });
+
+            ImGuiEx.ImGuiLineCentered("AboutHeaderCN", delegate
+            {
+                ImGuiEx.Text($"汉化 ");
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudRed, FontAwesomeIcon.Heart.ToIconString());
+                ImGui.PopFont();
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text($" by MeowZ");
+            });
         }
 
         static void WithLoveByOther()
@@ -43,6 +54,17 @@ namespace PunishLib
                 ImGui.PopFont();
                 ImGui.SameLine(0, 0);
                 ImGuiEx.Text($" by {PunishLibMain.About.Developer}, published by Puni.sh");
+            });
+
+            ImGuiEx.ImGuiLineCentered("AboutHeaderCN", delegate
+            {
+                ImGuiEx.Text($"汉化 ");
+                ImGui.PushFont(UiBuilder.IconFont);
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text(ImGuiColors.DalamudRed, FontAwesomeIcon.Heart.ToIconString());
+                ImGui.PopFont();
+                ImGui.SameLine(0, 0);
+                ImGuiEx.Text($" by MeowZ");
             });
         }
     }
